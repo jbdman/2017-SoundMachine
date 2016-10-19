c0fcfa781c712c6792d86b061754256c385f7035
MrNick <navtges@gmail.com>
2016-10-18 21:55:38 -0400

    SoundServer

---
+++ src/Player.java
@@ -0,0 +1,47 @@
+import java.io.IOException;
+import java.net.*;
+
+import javax.sound.sampled.*;
+import javax.swing.*;
+
+public class Player extends JFrame {
+
+	public Player() {
+		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		this.setTitle("Test Sound Clip");
+		this.setSize(300, 200);
+		this.setVisible(true);
+
+		try {
+			// Open an audio input stream.
+			URL url = this.getClass().getClassLoader().getResource("steam-train-horn.wav");
+			AudioInputStream audioIn = AudioSystem.getAudioInputStream(url);
+			// Get a sound clip resource.
+			Clip clip = AudioSystem.getClip();
+			// Open audio clip and load samples from the audio input stream.
+			clip.open(audioIn);
+			clip.start();
+
+//			System.out.println("first time");
+//			for (int i=0; i<500000; i++) {
+//				System.out.println("test");
+//			}
+//			System.out.println("second time");
+//			audioIn = AudioSystem.getAudioInputStream(url);
+//			clip.close();
+//			// Open audio clip and load samples from the audio input stream.
+//			clip.open(audioIn);
+//			clip.start();
+//
+//			System.out.println("third time");
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	public static void main(String[] args) {
+		new Player();
+	}
+
+}
---
+++ src/SoundServer.java
@@ -0,0 +1,24 @@
+import java.net.*;
+import java.io.*;
+
+public class SoundServer {
+	public static void main() throws IOException {
+
+		// FMS Restricts us to 5800 - 5810
+		int portNumber = 5800;
+
+		try (ServerSocket serverSocket = new ServerSocket(portNumber);
+				Socket clientSocket = serverSocket.accept();
+				PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
+				BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));) {
+			String inputLine;
+			while ((inputLine = in.readLine()) != null) {
+				out.println(inputLine);
+			}
+		} catch (IOException e) {
+			System.out.println(
+					"Exception caught when trying to listen on port " + portNumber + " or listening for a connection");
+			System.out.println(e.getMessage());
+		}
+	}
+}
