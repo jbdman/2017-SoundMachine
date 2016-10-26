e39efc2d2ce39a4e6368060c5bdda5017a805748
MrNick <navtges@gmail.com>
2016-10-26 09:22:35 -0400

    Updates for working TCP based version

--- src/org/usfirst/frc/team88/util/Player.java
+++ src/org/usfirst/frc/team88/util/Player.java
@@ -13,37 +13,57 @@ public class Player extends JFrame {
 		this.setTitle("Test Sound Clip");
 		this.setSize(300, 200);
 		this.setVisible(true);
+	}
 
-		try {
-			// Open an audio input stream.
-			URL url = this.getClass().getClassLoader().getResource("steam-train-horn.wav");
-			AudioInputStream audioIn = AudioSystem.getAudioInputStream(url);
-			// Get a sound clip resource.
-			Clip clip = AudioSystem.getClip();
-			// Open audio clip and load samples from the audio input stream.
-			clip.open(audioIn);
-			clip.start();
+	public void playSound(String fileNickname){
+		URL url = null;
+		
+		System.out.println("fileNickname = " + fileNickname);
+		// Open an audio input stream.
+		
+		if(fileNickname.startsWith("horn")){
+			url = this.getClass().getClassLoader().getResource("sounds/steam-train-horn.wav");
+		}
+		if(fileNickname.startsWith("game_over")){
+			url = this.getClass().getClassLoader().getResource("sounds/game_over.wav");
+		}
 
-//			System.out.println("first time");
-//			for (int i=0; i<500000; i++) {
-//				System.out.println("test");
-//			}
-//			System.out.println("second time");
-//			audioIn = AudioSystem.getAudioInputStream(url);
-//			clip.close();
-//			// Open audio clip and load samples from the audio input stream.
-//			clip.open(audioIn);
-//			clip.start();
-//
-//			System.out.println("third time");
+		if(fileNickname.startsWith("shovel")){
+			url = this.getClass().getClassLoader().getResource("sounds/working-with-shovel.wav");
+		}
+
+		if(url == null){
+			url = this.getClass().getClassLoader().getResource("sounds/working-with-shovel.wav");
+		}
+
+		
+		if (url != null) {
 			
-		} catch (Exception e) {
-			e.printStackTrace();
+			try {
+				AudioInputStream audioIn = AudioSystem.getAudioInputStream(url);
+				// Get a sound clip resource.
+				Clip clip = AudioSystem.getClip();
+				// Open audio clip and load samples from the audio input stream.
+				clip.open(audioIn);
+				clip.setFramePosition(0);
+				clip.start();
+				
+	//			System.out.println("first time");
+	//			for (int i=0; i<500000; i++) {
+	//				System.out.println("test");
+	//			}
+	//			System.out.println("second time");
+	//			audioIn = AudioSystem.getAudioInputStream(url);
+	//			clip.close();
+	//			// Open audio clip and load samples from the audio input stream.
+	//			clip.open(audioIn);
+	//			clip.start();
+	//
+	//			System.out.println("third time");
+				
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
 		}
 	}
-
-	public static void main(String[] args) {
-		new Player();
-	}
-
 }
--- src/org/usfirst/frc/team88/util/SoundServer.java
+++ src/org/usfirst/frc/team88/util/SoundServer.java
@@ -4,11 +4,15 @@ import java.net.*;
 import java.io.*;
 
 public class SoundServer {
-	public static void main() throws IOException {
+	public static void main(String[] args) throws IOException {
 
 		// FMS Restricts us to 5800 - 5810
 		int portNumber = 5800;
+		Player soundPlayer = new Player();
 
+		System.out.println("SoundServer started.");
+
+		
 		try (ServerSocket serverSocket = new ServerSocket(portNumber);
 				Socket clientSocket = serverSocket.accept();
 				PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
@@ -16,7 +20,9 @@ public class SoundServer {
 			String inputLine;
 			while ((inputLine = in.readLine()) != null) {
 				out.println(inputLine);
+					soundPlayer.playSound(inputLine);
 			}
+			
 		} catch (IOException e) {
 			System.out.println(
 					"Exception caught when trying to listen on port " + portNumber + " or listening for a connection");
