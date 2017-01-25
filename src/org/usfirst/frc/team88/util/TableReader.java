2621a84c9fb1fd120bd4d1af8dfa8eaed6cc0c62
MrNick <navtges@gmail.com>
2017-01-25 15:02:51 -0500

    Update to play wav files Add build script

--- src/org/usfirst/frc/team88/util/TableReader.java
+++ src/org/usfirst/frc/team88/util/TableReader.java
@@ -1,8 +1,5 @@
 package org.usfirst.frc.team88.util;
 
-import edu.wpi.first.wpilibj.networktables.NetworkTable;
-import edu.wpi.first.wpilibj.tables.ITable;
-import edu.wpi.first.wpilibj.tables.ITableListener;
 import java.io.*;
 
 import javax.sound.sampled.AudioFormat;
@@ -10,112 +7,83 @@ import javax.sound.sampled.AudioSystem;
 import javax.sound.sampled.LineUnavailableException;
 import javax.sound.sampled.SourceDataLine;
 
-public class TableReader implements ITableListener{
+import edu.wpi.first.wpilibj.networktables.NetworkTable;
+import edu.wpi.first.wpilibj.tables.ITable;
+import edu.wpi.first.wpilibj.tables.ITableListener;
 
+public class TableReader implements ITableListener {
+	private Player player;
+	
 	public static void main(String[] args) throws IOException, LineUnavailableException {
-
 		new TableReader().run();
 	}
-
+	
+	public TableReader () {
+		player = new Player();
+	}
+	
 	public void run() throws LineUnavailableException {
-
-		double x;
-		double y;
-		double z;
-		double leftCurrent;
-		double rightCurrent;
-
-		final AudioFormat af =
-				new AudioFormat(Tone.SAMPLE_RATE, 8, 1, true, true);
+		double avgCurrent;
+		
+		final AudioFormat af = new AudioFormat(Tone.SAMPLE_RATE, 8, 1, true, true);
 		SourceDataLine line = AudioSystem.getSourceDataLine(af);
-		SourceDataLine line2 = AudioSystem.getSourceDataLine(af);
 		line.open(af, Tone.SAMPLE_RATE);
 		line.start();
-		line2.open(af, Tone.SAMPLE_RATE);
-		line2.start();
 
 		System.out.println("Network Reader started.");
 
 		NetworkTable.setClientMode();
 		NetworkTable.setIPAddress("10.88.88.20");
-		NetworkTable table = NetworkTable.getTable("oitable");
+		NetworkTable robotTable = NetworkTable.getTable("robot");
+		robotTable.addTableListener(this);
 
-		while(true){
-			try{
+		while (true) {
+			try {
 				Thread.sleep(50);
+			} catch (InterruptedException ex) {
+				// Logger.getLogger(NetworkTablesDesktopClient.class.getName())log(Level.SEVERE,
+				// null, ex);
 			}
-			catch(InterruptedException ex){
-				//				Logger.getLogger(NetworkTablesDesktopClient.class.getName())log(Level.SEVERE, null, ex);
-			}
-			x = table.getNumber("x", 0.0);
-			y = table.getNumber("y", 0.0);
-			z = table.getNumber("z", 0.0);
-			
-			leftCurrent = table.getNumber("Left Current", 0.0);
-			rightCurrent = table.getNumber("Right Current", 0.0);
 
-			boolean shifted = table.getBoolean("Has Shifted", false);
-			if(shifted){
-				playSound(line, 50);
-			}
-			else{
-				playSound(line, 15);
-			}
-//			System.out.println("x: " + x + " y: " + y + " z: " + z + " Right " + rightCurrent + " Left " + leftCurrent);
-//			System.out.println();
-						
-			//right sound
-			playSound(line, rightCurrent);
-			//left sound
-			playSound(line2, leftCurrent);
+			avgCurrent = robotTable.getNumber("driveAvgCurrent", 0.0);
+
+			playSound(line, avgCurrent);
 		}
 	}
 
-	private static void playSound(SourceDataLine line, double current){
+	private static void playSound(SourceDataLine line, double current) {
 
-		if(current > 30){
+		if (current > 30) {
 			play(line, Tone.A5, 50);
-		}
-		else if(current > 27.5){
+		} else if (current > 27.5) {
 			play(line, Tone.G4$, 50);
-		}
-		else if(current > 25){
+		} else if (current > 25) {
 			play(line, Tone.G4, 50);
-		}
-		else if(current > 22.5){
+		} else if (current > 22.5) {
 			play(line, Tone.F4$, 50);
-		}
-		else if(current > 20){
+		} else if (current > 20) {
 			play(line, Tone.F4, 50);
-		}
-		else if(current > 17.5){
+		} else if (current > 17.5) {
 			play(line, Tone.E4, 50);
-		}
-		else if(current > 15){
+		} else if (current > 15) {
 			play(line, Tone.D4$, 50);
-		}
-		else if(current > 12.5){
+		} else if (current > 12.5) {
 			play(line, Tone.D4, 50);
-		}
-		else if(current > 10){
+		} else if (current > 10) {
 			play(line, Tone.C4$, 50);
-		}
-		else if(current > 7.5){
+		} else if (current > 7.5) {
 			play(line, Tone.C4, 50);
-		}
-		else if(current > 5){
+		} else if (current > 5) {
 			play(line, Tone.B4, 50);
-		}
-		else if(current > 2.5){
+		} else if (current > 2.5) {
 			play(line, Tone.A4$, 50);
-		}
-		else if(current > 0){
+		} else if (current > 0) {
 			play(line, Tone.A4, 50);
-		}
-		else{
+		} else {
 			play(line, Tone.REST, 50);
 		}
 	}
+
 	private static void play(SourceDataLine line, Tone note, int ms) {
 		ms = Math.min(ms, Tone.SECONDS * 1000);
 		int length = Tone.SAMPLE_RATE * ms / 1000;
@@ -123,8 +91,17 @@ public class TableReader implements ITableListener{
 	}
 
 	@Override
-	public void valueChanged(ITable arg0, String arg1, Object arg2, boolean arg3) {
-		// TODO Auto-generated method stub
+	public void valueChanged(ITable table, String key, Object value, boolean isNew) {
+		switch (key) {
+		case "inLow":
+			if ((boolean) value) {
+				// play shifted to low gear sound
+				player.playSound("shovel");
+			} else {
+				// play shifted to high gear sound
+				player.playSound("horn");
+			}
+		}
 
 	}
 
@@ -143,9 +120,9 @@ enum Tone {
 			double exp = ((double) n - 1) / 12d;
 			double f = 220d * Math.pow(2d, exp);
 			for (int i = 0; i < sin.length; i++) {
-				double period = (double)SAMPLE_RATE / f;
+				double period = (double) SAMPLE_RATE / f;
 				double angle = 2.0 * Math.PI * i / period;
-				sin[i] = (byte)(Math.sin(angle) * 127f);
+				sin[i] = (byte) (Math.sin(angle) * 127f);
 			}
 		}
 	}
